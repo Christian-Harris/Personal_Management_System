bc6bc29968153a6cb0bfe8da5832d64557a2fc59
Christian Harris <harris.chrw@gmail.com>
2020-08-26 15:52:03 -0600

    Create Item.java

---
+++ src/pmsPackage/Item.java
@@ -0,0 +1,19 @@
+package pmsPackage;
+
+public class Item {
+	private String item = "";
+	
+	public Item() {}
+	
+	public Item(String s) {
+		item = s;
+	}
+	
+	public String getItem() {
+		return this.item;
+	}
+	
+	public void setItem(String s) {
+		item = s;
+	}
+}
